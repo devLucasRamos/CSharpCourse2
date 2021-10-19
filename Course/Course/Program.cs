b1647aeae5a66bef05667ba377596fed1ac07f73
Lucas Ramos <lucas2adriano@gmail.com>
2021-10-19 13:22:41 -0300

    Revendo conceitos!

---
+++ Course/Course/Program.cs
@@ -0,0 +1,13 @@
+﻿
+using System;
+
+namespace Course
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
